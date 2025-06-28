645829493198be850abc5819ddd7c588f6206e46
lolu1032 <soowkw@naver.com>
2025-06-28 23:46:39 +0900

    비관적 락을 걸며 생긴 톰캣 스레드 놀고있음

--- src/get_coupon.js
+++ src/get_coupon.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-    vus: 3000, // 동시 유저 수
+    vus: 1000, // 동시 유저 수
     duration: '1s', // 1초에 몰아넣기
 };
 
@@ -10,7 +10,7 @@ export default function () {
     const userId = __VU - 1;
 
     const payload = JSON.stringify({
-        id: 20,  // 쿠폰 ID
+        id: 27,  // 쿠폰 ID
         userId: userId,
     });
 
@@ -23,5 +23,5 @@ export default function () {
     });
 
     console.log(`User ${userId} tried to get coupon - status: ${res.status}`);
-    sleep(1);
+    sleep(0.1);
 }
--- src/main/java/com/example/side_project/repository/CouponIssuesRepository.java
+++ src/main/java/com/example/side_project/repository/CouponIssuesRepository.java
@@ -4,5 +4,4 @@ import com.example.side_project.domain.Coupon_issues;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponIssuesRepository extends JpaRepository<Coupon_issues, Long> {
-    boolean existsByUserIdAndCouponId(Long userId, Long couponId);
 }
