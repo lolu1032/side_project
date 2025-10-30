4ffc8bf7eafb8aac67f57a1589abbdee0ef343f6
lolu1032 <soowkw@naver.com>
2025-10-30 18:45:08 +0900

    테스트03

--- src/main/java/com/example/sideProject/config/OpenApiConfig.java
+++ src/main/java/com/example/sideProject/config/OpenApiConfig.java
@@ -41,9 +41,13 @@ public class OpenApiConfig {
             openApi.setTags(Arrays.asList(
                     new Tag().name("User").description("사용자 관련 API"),
                     new Tag().name("Coupon_Row_lock").description("쿠폰 API(row lock)"),
-                    new Tag().name("Coupon_Synchronized").description("쿠폰 API(synchronized)"),
-                    new Tag().name("Coupon_Atomic").description("쿠폰 API(Atomic)"),
-                    new Tag().name("Coupon_Redis").description("쿠폰 API (Redis)"),
+                    new Tag().name("Coupon").description("""
+        쿠폰 통합 API promotion : 1~10, userId : 1
+        type
+        - redis: Redis 기반 재고 처리
+        - synchronized: 서버 메모리 기반 동기화 처리
+        - atomic: AtomicInteger 기반 경쟁 제어 처리
+    """),
                     new Tag().name("Coupon Queue").description("쿠폰 대기열 API (Redis)")
             ));
         };
